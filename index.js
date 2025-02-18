58321e118cb8fda96d2810dfe6d74662081c912d
Thellezie <llfernandez4@up.edu.ph>
2025-02-18 14:56:28 +0800

    feat: validate password function

--- index.js
+++ index.js
@@ -12,47 +12,6 @@ purpose of use: to convert characters to ASCII values
 */
 
 // ------ Functions ------ //
-//Function: check if the password contains 1 num, 1 uppercase, 1 lowercase 
-function checker(string){
-    let num = false
-    let upper = false
-    let lower = false
-
-
-    for(let i = 0; i < string.length; i++) {
-        // number
-        if(string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57){
-            num = true
-        } 
-        // uppercase
-        else if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
-            upper = true
-        } 
-        // lowercase
-        else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
-            lower = true
-        }
-    }
-
-    // If all conditions are met return true, otherwise false
-    if (num && upper && lower){
-        return true
-    }else{
-        return false
-    }
-
-
-}
-
-// Function: Reversing the pasword
-function reversePass(stringToReverse){
-    let reversed = ''
-    for (let i = stringToReverse.length-1; i >= 0; i--){
-        reversed += stringToReverse[i]
-    }
-
-    return reversed
-}
 
 //Function: validating if 2 passwords match
 function validatePassword(StringA, StringB){
@@ -70,37 +29,3 @@ function validatePassword(StringA, StringB){
     }
 }
 
-// Function: Store password to the object
-function  storePassword (Name, password1, password2){
-    let newPassword
-
-    // if conditions are met for passwords: use reversed password as new password, otherwise use the 1st given password
-    if(validatePassword(password1, password2)){
-        newPassword = reversePass(password1)
-
-    }else{
-        newPassword = password1
-    }
-
-    // Object
-    const person = {
-        name: Name,
-        newpassword: newPassword
-    }
-
-    return person
-}
-
-// ------ Main Function ------ //
-
-console.log(storePassword("John", "Pass1234", "Pass1234"))
-console.log(storePassword("John", "Pass123", "Pass12345"))
-
-/*
-// Test cases
-//ValidatePassword
-console.log(validatePassword("helloworld", "hello"))
-console.log(validatePassword("hello", "hello"))
-console.log(validatePassword("hello1234", "hello1234"))
-console.log(validatePassword("Hello1234", "Hello1234"))
-*/
